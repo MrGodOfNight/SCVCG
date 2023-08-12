ed58497b776bb541960510d2735c4d72e6e93920
MrGodOfNight <DenRussianBoy@mail.ru>
2023-08-12 23:18:26 +0500

    feat(all): library redesign preparations.

--- include/SCVCG.h
+++ include/SCVCG.h
@@ -25,67 +25,6 @@ extern "C" {
 
 
 
-typedef struct _TCPsocket *TCPsocket;
-
-/*
-* library initialization
-*/
-int SCVCG_init();
-
-
-
-// all server functions:
-
-/**
-* server function to send sound from one user to another
-* @param sender sender socket
-* @param recipient recipient socket
-*/
-void SCVCG_2D_server_send(TCPsocket sender, TCPsocket recipient);
-
-
-
-//all client functions:
-
-/**
-* client-server connection function. Function that takes the string ip and the number port as arguments, and returns an object of type TCPsocket
-* @param ip Server IP
-* @param port Server Port
-*/
-TCPsocket SCVCG_connect(char* ip, int port);
-
-/**
-* start sending sound to server
-* @param sender sender socket
-*/
-void SCVCG_2D_start_send(TCPsocket sender);
-
-/**
-* stopping the Audio Receive Stream
-* @param sender sender socket
-*/
-void SCVCG_2D_stop_send(TCPsocket sender);
-
-/**
-* starting an Audio Playback Stream
-* @param recipient recipient socket
-* @param volume volume from 0 to 128
-*/
-void SCVCG_2D_start_receive(TCPsocket recipient, int volume);
-
-/**
-* stop audio playback stream
-* @param recipient recipient socket
-*/
-void SCVCG_2D_stop_receive(TCPsocket recipient);
-
-/*
-* releasing resources occupied by the library
-*/
-void SCVCG_cleanup();
-
-
-
 #if defined(__cplusplus)
 }  /* extern "C" */
 #endif
--- src/SCVCG.c
+++ src/SCVCG.c
@@ -20,71 +20,8 @@
 * including headers of the library itself
 */
 #include "../include/SCVCG.h"
-#include "headers/server.h"
-#include "headers/record.h"
-#include "headers/reproduction.h"
-#include "headers/client.h"
 
 /*
 * connecting various headers and libs
 */
-#include <al.h>
-#include <alc.h>
-#include <SDL.h>
-#include <SDL_mixer.h>
-#include <SDL_net.h>
 
-
-
-int SCVCG_init()
-{
-  //initialize the library SDL
-  if(SDL_Init(SDL_INIT_EVERYTHING) != 0) 
-  {
-    printf("SDL_Init Error: %s", SDL_GetError());
-    return 1;
-  }
-  
-  //initialize the library SDL_mixer
-  if(Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, AUDIO_S16SYS, 2, 1024) != 0) 
-  {
-    printf("Mix_OpenAudio Error: %s", Mix_GetError());
-    return 1;
-  }
-
-  //initialize the library SDL_net
-  if(SDLNet_Init() != 0) 
-  {
-    printf("SDLNet_Init Error: %s", SDLNet_GetError());
-    return 1;
-  }
-  
-  //initialize the library OpenAL
-  ALCdevice *device = alcOpenDevice(NULL);
-  if (device == NULL) 
-  {
-    return -1;
-  }
-
-  ALCcontext *context = alcCreateContext(device, NULL);
-  if (context == NULL) 
-  {
-    return -1;
-  }
-
-  alcMakeContextCurrent(context);
-
-  return 0;
-}
-
-void SCVCG_cleanup()
-{
-  Mix_Quit();
-  SDLNet_Quit();
-  SDL_Quit();
-  ALCcontext *context = alcGetCurrentContext();
-  ALCdevice *device = alcGetContextsDevice(context);
-  alcMakeContextCurrent(NULL);
-  alcDestroyContext(context);
-  alcCloseDevice(device);
-}
